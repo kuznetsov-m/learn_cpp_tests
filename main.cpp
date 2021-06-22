1b92e594bc1b3a47b1ab2f6579c7507e28c1f1e0
Mikhail Kuznetsov <61391551+kuznetsov-m@users.noreply.github.com>
2021-06-22 18:20:52 +0300

    main.cpp add

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
+// #include <iostream>
+
+// int main()
+// {
+//     std::cout << "Hello world\n";
+// }
+// ---------------------------------------------
+
 #include <iostream>
+#include <vector>
+#include <string> 
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello world\n";
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
 }
